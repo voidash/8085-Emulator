3f36f093e26a37e3e3df0f17989fa4faf446ea21
voidash <ashish.thapa477@gmail.com>
2022-04-05 08:58:11 +0545

    modularized frontend code: new file for 8085 tokenizer , each component now has its own folder :rocket:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,44 +3,17 @@ import logo from './logo.svg';
 import { useEffect } from 'react';
 
 import Editor, {useMonaco} from "@monaco-editor/react";
-import Flags from './components/flags';
-import Registers from './components/registers';
-
+import Flags from './components/Flags/flags';
+import Registers from './components/Register/registers';
+import EightOEightFiveDefinition from './monaco/languageDefinition';
+import MemoryView from './components/MemoryView/memory_view';
 
 function App() {
   const monaco = useMonaco();
   useEffect(() => {
       if (monaco) {
         monaco.languages.register({id: '8085asm'});
-        monaco.languages.setMonarchTokensProvider('8085asm', {
-          digits: /\d+(?:_\d+)*[dD]?/,
-          octaldigits: /[0-7]+(_+[0-7]+)*/,
-          binarydigits: /[0-1]+(_+[0-1]+)*/,
-          hexdigits: /[[0-9a-fA-F]+(_+[0-9a-fA-F]+)*/,
-    registers: [],
-          keywords: ['di','sim','lhld','stc','aci','jnz','rm','mvi','rp','inr','hlt','rc','ani','xra','cc','nop','push','adc','add','ral','daa','rpe','cz','cmc','rst','xchg','rz','pop','jnc','rnc','rar','sta','rpo','xthl','call','cnc','in','cnz','dad','ei','mov','jmp','sbb','ora','pchl','ana','lda','sphl','cp','cma','cmp','xri','ori','cpi','jc','sub','dcr','cpe','rim','shld','cm','rrc','sbi','jpe','dcx','jz','rlc','ret','lxi','cpo','jpo','ldax','jp','inx','out','jm','sui','adi','stax','rnz','DI','SIM','LHLD','STC','ACI','JNZ','RM','MVI','RP','INR','HLT','RC','ANI','XRA','CC','NOP','PUSH','ADC','ADD','RAL','DAA','RPE','CZ','CMC','RST','XCHG','RZ','POP','JNC','RNC','RAR','STA','RPO','XTHL','CALL','CNC','IN','CNZ','DAD','EI','MOV','JMP','SBB','ORA','PCHL','ANA','LDA','SPHL','CP','CMA','CMP','XRI','ORI','CPI','JC','SUB','DCR','CPE','RIM','SHLD','CM','RRC','SBI','JPE','DCX','JZ','RLC','RET','LXI','CPO','JPO','LDAX','JP','INX','OUT','JM','SUI','ADI','STAX','RNZ','a', 'b', 'c', 'd', 'e', 'h', 'l', 'm', 'psw','A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'PSW', ],
-          tokenizer: {
-            root: [
-              [/[a-zA-Z][\w]*/, {
-                  cases: {
-                    '@keywords': 'keyword',
-                    '@default': 'identifier',
-                  }
-              }],
-
-              [/(@octaldigits)[qQ]/, 'number.octal'],
-              [/(@binarydigits)[bB]/, 'number.binary'],
-              [/0x(@hexdigits)[hH]?/, 'number.hexdigits'],
-              [/@digits/, 'number'],
-              {include: '@whitespace'}
-            ],
-
-            whitespace: [
-              [/[ \t\r\n]+/, ''],
-              [/;.*$/, 'comment']
-            ],
-          },
-        });
+        monaco.languages.setMonarchTokensProvider('8085asm', EightOEightFiveDefinition());
       }
   }, [monaco]);
   
@@ -49,6 +22,8 @@ function App() {
     <div>
       <Flags/>
       <Registers/>
+      <MemoryView/>
+
       <Editor
         height="90vh"
         width="50vw"
---
+++ frontend/src/components/MemoryView/memory_view.tsx
@@ -0,0 +1,5 @@
+import {useState} from 'react';
+
+export default function MemoryView() {
+  return(<div>this is memory View</div>);
+}
