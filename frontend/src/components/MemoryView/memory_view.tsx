ba66bc3de7fc205da97ddd67cb04e361fb96968c
voidash <ashish.thapa477@gmail.com>
2022-04-05 14:34:51 +0545

    add search bar

--- frontend/src/components/MemoryView/memory_view.tsx
+++ frontend/src/components/MemoryView/memory_view.tsx
@@ -1,5 +1,30 @@
 import {useState} from 'react';
+import './style.css';
 
+interface memory {
+  position: string,
+  value: string
+};
+
+let Memory = ({position, value}: memory) => {
+  return(
+    <div className = "MemoryBox">
+      <div className= "position">{position}</div>
+      <div className="value">{value}</div>
+    </div>
+  )
+}
 export default function MemoryView() {
-  return(<div>this is memory View</div>);
+  return(
+    <div className="mem">
+    <div className="MemorySearch">
+      <input type="text" className="searchBar"/>
+      <button className="searchButton">Search</button>
+    </div>
+      <div className="MemoryView">
+    <Memory position="0x22ff" value="0x33"/>
+    <Memory position="0x22ff" value="0x33"/>
+ </div>
+</div>);
+
 }
