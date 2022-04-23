4bc09cfb5b88ef5e9029e68cd7c3074c3401c9c2
voidash <ashish.thapa477@gmail.com>
2022-04-23 22:15:46 +0545

    add some extra parameters for binary and support for printing 8085 state

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,80 @@
 extern crate emulator;
 
-use emulator::generate_assembly_code;
+pub mod utils;
+pub mod core; 
+pub mod assembler; 
+
 
 use clap::Parser;
+use std::path::Path;
+use std::fs;
+
+use core::Processor8085;
+use utils::print_8085_state;
 
 
 #[derive(Parser, Debug)]
 #[clap(author, version)]
 struct Args {
+    /// If output is not provided then the program will assume writing to stdout
     #[clap(short,long)]
     output: Option<String>,
+    /// if filename is not passed then the program will run on interpreted mode
     filename: Option<String>,
+    /// run on interpreted mode
    #[clap(short, long, conflicts_with("filename"))]
-    interpreted: bool
+    interpreted: bool,
+    /// pass filename and generate assembly code 
+   #[clap(short, long,conflicts_with("interpreted"))]
+    assemble : bool,
+    /// pass .asm file and disassemble them
+   #[clap(short, long, conflicts_with("assemble"), conflicts_with("interpreted"))]
+    disassemble: bool,
+    /// emulate 8085 and write its state and memory values to file. 
+   #[clap(short, long, conflicts_with("assemble"),conflicts_with("disassemble"),conflicts_with("interpreted"))]
+    emulate: bool
 }
 
 #[allow(dead_code)]
-fn read_from_file() {
-
+fn read_from_file(file:&Path) -> Result<String,String> {
+    if let Ok(contents) = fs::read_to_string(file) {
+      return Ok(contents);
+    }
+    return Err(format!("couldn't read file"));
 }
+
 
 fn main() {
     let args = Args::parse();
-    let lines = vec![ 
-        "nop; Non arg line",
-        "test: mvi a, 45h     ",
-        "lda 3432h",
-        "jmp test"
-    ].iter().map(|&a| {
-        String::from(a) 
-    }).collect();
-    println!("this is will some helper functions to read from file and write to file");
+
+    // if filename is provided then write to file except write to stdout
+    let mut print_to_stdout = false;
+    match args.filename {
+        Some(file) => {
+            print_to_stdout = false;
+        }
+        None => {
+            print_to_stdout = true;
+        }
+    }
+
+    // if running on emulator mode file shouldn't contain
+    if args.emulate == true {
+        println!("running emulator");
+    }
+    if args.disassemble == true {
+
+    } 
+    if args.assemble == true {
+
+    }
+    if args.interpreted == true {
+
+    }
+
+    let s = Processor8085::new();
+    println!("{}", print_8085_state(s));
 
 
-    generate_assembly_code(lines);
-
-    println!("Hello {:?}",args);
 
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use crate::core::Processor8085;
+
 pub fn check_parity(data: u8 , size: u8) -> bool{
 let mut ones = 0;
 for i in 0..size {
@@ -16,7 +18,28 @@ pub fn bin_to_bool(data:u8) -> bool{
     if data == 1 {true} else {false}
 }
 
+pub fn print_8085_state(state: &Processor8085) -> String{ 
+    let output_string  =  format!(
+        r#" 
+|-------------|-------------|
+|b: {:x}      | c: {:x}     | 
+|-------------|-------------|
+|d: {:x}      | e: {:x}     | 
+|-------------|-------------|
+|h: {:x}      | l: {:x}     | 
+|-------------|-------------|
+|stack pointer:  {:x}       | 
+|-------------|-------------|
+|program counter:  {:x}     | 
+|-------------|-------------|
+|s: {},z:{},AC:{},P:{},CY:{}| 
+|-------------|-------------|
+        "#
+    ,state.b, state.c, state.d, state.e, state.h, state.l, state.stack_pointer, state.program_counter
+    , state.flag.sign, state.flag.zero, state.flag.auxillary_carry, state.flag.parity, state.flag.carry);
 
+    output_string
+}
 
 #[test]
 fn parity_of_10() {
